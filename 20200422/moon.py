0b9dd361f08d773e059c46bfb790ce8c2a188daf
sanghoon kim <kshf59@naver.com>
2020-04-22 22:28:53 +0900

    '''

---
+++ 20200422/moon.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = 1
+
+
+# In[2]:
+
+
+_a = 2
+
+
+# In[3]:
+
+
+__a = 3
+
+
+# In[4]:
+
+
+def b():
+    return 1
+
+
+# In[5]:
+
+
+def _b():
+    return 2
+
+
+# In[6]:
+
+
+def _c():
+    return 3
+
+
+# In[7]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
